dde38d884d520a5547b2cd6ee9e218bb0760b98c
DevFecta <devfecta@gmail.com>
2024-04-06 09:09:28 -0500

    updated homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,15 @@ class Home extends Component {
         return (
             <section className='row justify-content-between'>
 				<div className='col-12 col-md-7 d-flex align-items-center'>
-					<p className='fs-2'>PN Nail Salon offers manicures and pedicures from professional nail technicians to the Madison area.</p>
+                    <div className='col-12'>
+                        <p className='fs-3'>
+                        At PN Nail Salon, we offer manicures and pedicures from professional nail technicians to the Madison area. 
+                        Whether you are visiting the salon for the first time or are a loyal guest, it is our goal to give you 
+                        beautiful nails while you enjoy a rejuvenating and relaxing experience.</p>
+                        <p className='fs-4 fst-italic'>
+                        All services are performed by our highly trained and licensed staff. 
+                        </p>
+                    </div>
 				</div>
                 <div className='col-12 col-md-5'>
                     <Image src='images/manicure-pedicure.jpg' fluid />
